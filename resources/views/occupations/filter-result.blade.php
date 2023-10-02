572a068d1c77e5062c9ed662e29b6403acaa33e9
walfrido-oliveira <walfrido_15@hotmail.com>
2023-10-02 13:41:08 -0300

    Ajustes Iniciais CRM #25

--- resources/views/occupations/filter-result.blade.php
+++ resources/views/occupations/filter-result.blade.php
@@ -11,13 +11,13 @@
     </tr>
 </thead>
 <tbody id="occupations_table_content">
-    @forelse ($occupations as $key => $occupations)
+    @forelse ($occupations as $key => $occupation)
         <tr>
             <td>
-                <input class="form-checkbox occupations-url" type="checkbox" name="occupations[{{ $occupations->id }}]" value="{!! route('occupations.destroy', ['occupation' => $occupations->id]) !!}">
+                <input class="form-checkbox occupations-url" type="checkbox" name="occupations[{{ $occupation->id }}]" value="{!! route('occupations.destroy', ['occupation' => $occupation->id]) !!}">
             </td>
             <td>
-                <a class="text-item-table" href="{{ route('occupations.show', ['occupation' => $occupations->id]) }}">{{ $occupations->name }}</a>
+                <a class="text-item-table" href="{{ route('occupations.show', ['occupation' => $occupation->id]) }}">{{ $occupation->name }}</a>
             </td>
             <td>
                 <a class="text-item-table" href="{{ route('occupations.show', ['occupation' => $occupation->id]) }}">{{ $occupation->created_at->format('d/m/Y') }}</a>
@@ -26,12 +26,12 @@
                 <a class="text-item-table" href="{{ route('occupations.show', ['occupation' => $occupation->id]) }}">{{ $occupation->updated_at->format('d/m/Y') }}</a>
             </td>
             <td>
-                <a class="btn-transition-warning" href="{{ route('occupations.edit', ['occupation' => $occupations->id]) }}">
+                <a class="btn-transition-warning" href="{{ route('occupations.edit', ['occupation' => $occupation->id]) }}">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                     </svg>
                 </a>
-                <button class="btn-transition-danger delete-occupations" data-url="{!! route('occupations.destroy', ['occupation' => $occupations->id]) !!}">
+                <button class="btn-transition-danger delete-occupations" data-url="{!! route('occupations.destroy', ['occupation' => $occupation->id]) !!}">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                     </svg>
