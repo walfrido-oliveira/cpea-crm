2db38ab051e902257854e3c9f81150ea807cd433
walfrido-oliveira <walfrido_15@hotmail.com>
2024-04-25 07:39:42 -0300

    correção

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,76 +1,78 @@
 <x-app-layout>
-    <div class="py-6 index-customers">
-        <div class="md:max-w-6xl lg:max-w-full mx-auto px-4">
+  <div class="py-6 index-customers">
+    <div class="md:max-w-6xl lg:max-w-full mx-auto px-4">
 
-            <div class="flex md:flex-row flex-col">
-                <div class="w-full flex items-center">
-                    <h1>{{ __('Cliente') }}</h1>
-                </div>
-                <div class="w-full flex justify-end">
-                    <div class="m-2 ">
-                        <a class="btn-outline-info" href="{{ route('customers.create') }}" >{{ __('Cadastrar') }}</a>
-                    </div>
-                    <div class="m-2">
-                        <button type="button" class="btn-outline-danger delete-customers" data-type="multiple">{{ __('Apagar') }}</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="py-2 my-2 bg-white rounded-lg min-h-screen">
-                <div class="filter-container">
-                    <div class="flex -mx-3 mb-6 p-3 md:flex-row flex-col w-full">
-                        <div class="w-full md:w-1/2 px-2 mb-6 md:mb-0">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="id">
-                                {{ __('Cód Cliente') }}
-                            </label>
-                            <x-jet-input id="id" class="form-control block w-full filter-field" type="text" name="id" :value="app('request')->input('id')" autofocus autocomplete="id" />
-                        </div>
-                        <div class="w-full md:w-1/2 px-2 mb-6 md:mb-0">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name">
-                                {{ __('Cliente') }}
-                            </label>
-                            <x-jet-input id="name" class="form-control block w-full filter-field" type="text" name="name" :value="app('request')->input('name')" autofocus autocomplete="name" />
-                        </div>
-                        <div class="w-full md:w-1/2 px-2 mb-6 md:mb-0">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="segment_id">
-                                {{ __('Segmento') }}
-                            </label>
-                            <x-custom-select class="mt-1" :options="$segments" name="segment_id" id="segment_id" value=""/>
-                        </div>
-                        <div class="w-full md:w-1/2 px-2 mb-6 md:mb-0">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="status">
-                                {{ __('Situação') }}
-                            </label>
-                            <x-custom-select class="mt-1" :options="$status" name="status" id="status" value=""/>
-                        </div>
-                        <div class="w-full md:w-1/2 px-2 mb-6 md:mb-0">
-                            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="new_customer">
-                                {{ __('Novo cliente?') }}
-                            </label>
-                            <x-custom-select class="mt-1" :options="$isNewCustomer" name="new_customer" id="new_customer" value=""/>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex mt-4">
-                    <table id="customers_table" class="table table-responsive md:table w-full">
-                        @include('customers.filter-result', ['customers' => $customers, 'ascending' => $ascending, 'orderBy' => $orderBy])
-                    </table>
-                </div>
-                <div class="flex mt-4 p-2" id="pagination">
-                    {{ $customers->appends(request()->input())->links() }}
-                </div>
-            </div>
+      <div class="flex md:flex-row flex-col">
+        <div class="w-full flex items-center">
+          <h1>{{ __('Cliente') }}</h1>
         </div>
+        <div class="w-full flex justify-end">
+          <div class="m-2 ">
+            <a class="btn-outline-info" href="{{ route('customers.create') }}">{{ __('Cadastrar') }}</a>
+          </div>
+          <div class="m-2">
+            <button type="button" class="btn-outline-danger delete-customers" data-type="multiple">{{ __('Apagar')
+              }}</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="py-2 my-2 bg-white rounded-lg min-h-screen">
+        <div class="filter-container">
+          <div class="flex -mx-3 mb-6 p-3 md:flex-row flex-col w-full">
+            <div class="w-full md:w-1/2 px-2 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="id">
+                {{ __('Cód Cliente') }}
+              </label>
+              <x-jet-input id="id" class="form-control block w-full filter-field" type="text" name="id"
+                :value="app('request')->input('id')" autofocus autocomplete="id" />
+            </div>
+            <div class="w-full md:w-1/2 px-2 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name">
+                {{ __('Cliente') }}
+              </label>
+              <x-jet-input id="name" class="form-control block w-full filter-field" type="text" name="name"
+                :value="app('request')->input('name')" autofocus autocomplete="name" />
+            </div>
+            <div class="w-full md:w-1/2 px-2 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="segment_id">
+                {{ __('Segmento') }}
+              </label>
+              <x-custom-select class="mt-1" :options="$segments" name="segment_id" id="segment_id" value="" />
+            </div>
+            <div class="w-full md:w-1/2 px-2 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="status">
+                {{ __('Situação') }}
+              </label>
+              <x-custom-select class="mt-1" :options="$status" name="status" id="status" value="" />
+            </div>
+            <div class="w-full md:w-1/2 px-2 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="new_customer">
+                {{ __('Novo cliente?') }}
+              </label>
+              <x-custom-select class="mt-1" :options="$isNewCustomer" name="new_customer" id="new_customer" value="" />
+            </div>
+          </div>
+        </div>
+        <div class="flex mt-4">
+          <table id="customers_table" class="table table-responsive md:table w-full">
+            @include('customers.filter-result', ['customers' => $customers, 'ascending' => $ascending, 'orderBy' =>
+            $orderBy])
+          </table>
+        </div>
+        <div class="flex mt-4 p-2" id="pagination">
+          {{ $customers->appends(request()->input())->links() }}
+        </div>
+      </div>
     </div>
+  </div>
 
-    <x-modal title="{{ __('Excluir Cliente') }}"
-             msg="{{ __('Deseja realmente apagar esse Cliente?') }}"
-             confirm="{{ __('Sim') }}" cancel="{{ __('Não') }}" id="delete_customer_modal"
-             method="DELETE"
-             redirect-url="{{ route('customers.index') }}"/>
+  <x-modal title="{{ __('Excluir Cliente') }}" msg="{{ __('Deseja realmente apagar esse Cliente?') }}"
+    confirm="{{ __('Sim') }}" cancel="{{ __('Não') }}" id="delete_customer_modal" method="DELETE"
+    redirect-url="{{ route('customers.index') }}" />
 
-    <script>
-        window.addEventListener("load", function() {
+  <script>
+    window.addEventListener("load", function() {
             var filterCallback = function (event) {
                 var ajax = new XMLHttpRequest();
                 var url = "{!! route('customers.filter') !!}";
@@ -207,6 +209,6 @@
             eventsDeleteCallback();
             eventsFilterCallback();
         });
-    </script>
+  </script>
 
 </x-app-layout>
