ba04314c803d7c912ea927fc006839b64b7121bd
walfrido-oliveira <walfrido_15@hotmail.com>
2024-05-13 10:13:01 -0300

    Cadastro Rapido de Nova Interação na Tela Home #48

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,18 @@
 <x-app-layout>
-    <div class="py-6 dashboard">
-        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
-            <div class="p-4 sm:px-20 bg-white">
-                <p>Olá</p>
-            </div>
+  <div class="py-6 dashboard">
+    <div class="md:max-w-6xl lg:max-w-full mx-auto px-4">
+      <div class="flex md:flex-row flex-col">
+        <div class="w-full flex items-center">
+          <h1>{{ __('Dashboard') }}</h1>
         </div>
+        <div class="w-full flex justify-end">
+          <div class="m-2 ">
+            <a href="{{ route('customers.conversations.item.faster-create') }}" class="btn-outline-success">{{ __('Nova Interação') }}</a>
+          </div>
+        </div>
+      </div>
+      <div class="py-2 my-2 bg-white rounded-lg">
+      </div>
     </div>
+  </div>
 </x-app-layout>
