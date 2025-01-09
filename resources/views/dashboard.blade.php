100783ecaca383e1e8b11cdc00168db703719ed0
walfrido-oliveira <walfrido_15@hotmail.com>
2025-01-09 13:19:15 -0300

    Hide dashboard section with updated class for improved visibility control

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
           </div>
         </div>
       </div>
-      <div class="py-2 my-2 bg-white rounded-lg">
+      <div class="py-2 my-2 bg-white rounded-lg hidden" >
         <div class="flex flex-wrap mx-4 px-3 py-2 mt-0 justify-end">
           <div class="w-full md:w-1/6 px-2 mb-6 md:mb-0">
             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="year">
